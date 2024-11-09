725c35979f59b53940a739434b22dd1f277e97ac
Athena Boose <pestpestthechicken@yahoo.com>
2024-11-09 13:23:07 -0800

    Wrote maze generator, now to test it.

--- Assets/Scenes/Worlds/chickenbranchprogram/Maze.cs
+++ Assets/Scenes/Worlds/chickenbranchprogram/Maze.cs
@@ -25,10 +25,10 @@ public class Maze {
         for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < rows; ++j) {
                 visited_cells[i, j] = false;
-                left_wall[i, j] = false;
-                right_wall[i, j] = false;
-                up_wall[i, j] = false;
-                down_wall[i, j] = false;
+                left_wall[i, j] = true;
+                right_wall[i, j] = true;
+                up_wall[i, j] = true;
+                down_wall[i, j] = true;
 
             }
         }
@@ -47,21 +47,89 @@ public class Maze {
     }
 
     public void Visit(Cell cell) {
+        CheckBounds(cell);
         visited_cells[cell.GetRow(), cell.GetCol()] = true;
     }
 
-    public bool HasVisited(Cell cell) {
+    public bool HasVisited(int row, int col) {
+        Cell cell = new Cell(row, col);
+        CheckBounds(cell);
         return visited_cells[cell.GetRow(), cell.GetCol()];
     }
 
     public void BreakWall(Cell cell, Direction direction) {
+        CheckBounds(cell);
+        switch (direction) {
+            case Direction.Left:
+                if (cell.GetCol() == 0) {
+                    throw new ArgumentOutOfRangeException("Invalid cell and direction, wall cannot be broken.");
+                }
 
+                if (!left_wall[cell.GetRow(), cell.GetCol()]) {
+                    throw new InvalidOperationException("Wall has already been broken.");
+                }
+
+                left_wall[cell.GetRow(), cell.GetCol()] = false;
+                right_wall[cell.GetRow(), cell.GetCol() - 1] = false;
+                break;
+            case Direction.Right:
+                if (cell.GetCol() == cols) {
+                    throw new ArgumentOutOfRangeException("Invalid cell and direction, wall cannot be broken.");
+                }
+
+                if (!right_wall[cell.GetRow(), cell.GetCol()]) {
+                    throw new InvalidOperationException("Wall has already been broken.");
+                }
+
+                left_wall[cell.GetRow(), cell.GetCol() + 1] = false;
+                right_wall[cell.GetRow(), cell.GetCol()] = false;
+                break;
+            case Direction.Up:
+                if (cell.GetRow() == 0) {
+                    throw new ArgumentOutOfRangeException("Invalid cell and direction, wall cannot be broken.");
+                }
+
+                if (!up_wall[cell.GetRow(), cell.GetCol()]) {
+                    throw new InvalidOperationException("Wall has already been broken.");
+                }
+
+                up_wall[cell.GetRow(), cell.GetCol()] = false;
+                down_wall[cell.GetRow() - 1, cell.GetCol()] = false;
+                break;
+            case Direction.Down:
+                if (cell.GetRow() == rows) {
+                    throw new ArgumentOutOfRangeException("Invalid cell and direction, wall cannot be broken.");
+                }
+
+                if (!down_wall[cell.GetRow(), cell.GetCol()]) {
+                    throw new InvalidOperationException("Wall has already been broken.");
+                }
+
+                up_wall[cell.GetRow(), cell.GetCol()] = false;
+                down_wall[cell.GetRow() + 1, cell.GetCol()] = false;
+                break;
+        }
+    }
+
+    public bool CellHasWall(Cell cell, Direction direction) {
+        switch (direction) {
+            case Direction.Left:
+                return left_wall[cell.GetRow(), cell.GetCol()];
+            case Direction.Right:
+                return right_wall[cell.GetRow(), cell.GetCol()];
+            case Direction.Up:
+                return up_wall[cell.GetRow(), cell.GetCol()];
+            case Direction.Down:
+                return down_wall[cell.GetRow(), cell.GetCol()];
+        }
+
+        throw new ArgumentException("direction is invalid. ya dun fucked up. skill issue.");
     }
 
 
-    private void CheckBounds(Cell cell) {
+    public void CheckBounds(Cell cell) {
         if (cell.GetRow() >= rows || cell.GetRow() < 0 || cell.GetCol() >= cols || cell.GetCol() < 0) {
-            throw new ArgumentException();
+            throw new ArgumentOutOfRangeException("Cell is out of bounds.");
         }
     }
 }
