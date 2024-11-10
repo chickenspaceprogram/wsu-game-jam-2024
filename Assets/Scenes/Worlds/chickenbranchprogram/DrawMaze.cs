832bc8986b4edb31585e5a1943f676073d2b2d81
Athena Boose <pestpestthechicken@yahoo.com>
2024-11-10 10:38:48 -0800

    trying to figure out why i cannot make cube from c# script.

---
+++ Assets/Scenes/Worlds/chickenbranchprogram/DrawMaze.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawMaze : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject test = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
